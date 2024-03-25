892929f264d846a13df0d1f248bff15fbcc2e1bb
Fadi <130671609+Fadi-Mostefai@users.noreply.github.com>
2024-03-25 15:30:26 +0000

    Simplified exercises

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,16 +1,7 @@
 # Exercise 1
 # Your solution comes here
 def sum_coversion(num):
-  digit1 = 0
-  digit2 = 0
   str_num = str(num)
-  for i in range(1, len(str_num) + 1):
-    if i % 2 == 0:
-      digit2 += int(str_num[i-1])
-    else:
-      digit1 += int(str_num[i-1])
-  sum = str(digit1) + str(digit2)
-  return sum
-
-number = int(input("Enter a number "))
+  return str(int(str_num[0]) + int(str_num[2]) + int(str_num[4])) + str(int(str_num[1]) + int(str_num[3]))
+number = int(input(""))
 print(sum_coversion(number))
--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,10 +1,7 @@
 # Exercise 2
 # Your solution comes here
 def next_even(num):
-    if num % 2 == 0:
-        return num + 2
-    else:
-        return num + 1
+    return (num + 2) * (num % 2 == 0) + (num + 1) * (num % 2 != 0)
 
 num = int(input(""))
 print(next_even(num))
