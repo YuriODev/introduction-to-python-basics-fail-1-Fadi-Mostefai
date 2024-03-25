3ebf7b52fe5657cafea6e60d7ede6329311ba1b5
Fadi <130671609+Fadi-Mostefai@users.noreply.github.com>
2024-03-25 12:05:37 +0000

    change

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,7 +1,7 @@
 # Exercise 9
 # Your solution comes here
 def convert(h,m,s):
-    return (30 * h) + (6 * m) + (0.1 * s)
+    return (30 * h) + (0.5 * m) + ((0.5 / 60) * s)
 
 h = int(input(""))
 m = int(input(""))
