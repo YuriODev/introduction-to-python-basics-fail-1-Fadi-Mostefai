9bf6d245e1589650148a72823273d517661189fb
Fadi <130671609+Fadi-Mostefai@users.noreply.github.com>
2024-03-25 11:56:33 +0000

    Changes made

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,7 +1,7 @@
 # Exercise 5
 # Your solution comes here
 def answer(num1, num2):
-    return num1 * (num1 > num2) + num2 * (num2 > num1) + (num1, num2) * (num1 == num2)
+    return num1 * (num1 > num2) + num2 * (num2 > num1) + (num1 or num2) * (num1 == num2)
 
 num1 = int(input(""))
 num2 = int(input(""))
--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,4 +1,5 @@
 # Exercise 9
 # Your solution comes here
 def convert(h,m,s):
-    
+    return (30 * h) * (m==0 and s==0) + ((30 * h))
+
